5d22d36703db7f9565fb39f3991451bee06056d8
Швында Вадим Александрович <gr624_shvval@ttit.local>
2023-12-11 11:54:43 +0700

    not full 1.3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿string path = "../../../input.txt";
+string outputPath = "../../../output.txt";
+string[] arr = File.ReadAllLines(path);
+
+
+int[] winNum = arr[0].Split(' ').Select(x => int.Parse(x)).ToArray();
+int countTicket = int.Parse(arr[1]);
+int[][] tickets = new int[countTicket][];
+int index = 0;
+for (int i = 2; i < arr.Length; i++)
+{
+    tickets[index] = arr[i].Split(' ').Select(x => int.Parse(x)).ToArray();
+    index++;
+}
+
+File.WriteAllText(outputPath, "");
+foreach (int[] ticket in tickets)
+{
+    var result = winNum.Intersect(ticket);
+    if (result.Count() >= 3)
+    {
+        File.AppendAllText(outputPath, "Lucky \n");
+    }
+    else
+    {
+        File.AppendAllText(outputPath, "UnLucky \n");
+    }
+}
+
+
+
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,8 @@
+﻿string path = "../../../nums.txt";
+int[] nums = File.ReadAllLines(path)[0].Split(' ').Select(x => int.Parse(x)).ToArray();
+int[] ansNum = new int[];
+foreach (int num in nums)
+{
+    
+}
+foreach(int i in nums) Console.WriteLine(i);
