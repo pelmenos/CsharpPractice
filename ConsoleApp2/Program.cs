94638efd3476742ff1ec8a66c13ee656afa81df3
Швында Вадим Александрович <gr624_shvval@ttit.local>
2023-12-14 09:46:10 +0700

    full 1.3

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,8 +1,10 @@
 ﻿string path = "../../../nums.txt";
-int[] nums = File.ReadAllLines(path)[0].Split(' ').Select(x => int.Parse(x)).ToArray();
-int[] ansNum = new int[];
-foreach (int num in nums)
+List<int> nums = new List<int>();
+nums.AddRange(File.ReadAllLines(path)[0].Split(' ').Select(x => int.Parse(x)).ToArray());
+
+for (int i = 0; i < nums.Count; i++)
 {
-    
+    if (nums[i] % 2 == 0) nums.RemoveAt(i);
 }
+
 foreach(int i in nums) Console.WriteLine(i);
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿const int I = 9;
+int[] height = new int[I] {1,8,6,2,5,4,8,3,7};
+
+int V = 0;
+int x1 = 0;
+int x2 = height.Length - 1;
+int y = 0;
+for (int i = 0; i < height.Length / 2; i++)
+{
+    if (height[x1] >= height[x2]) y = height[x2];
+    else y = height[x1];
+
+    int x = (x2 - x1);
+
+    if (V < x * y)
+    {
+        V = x * y;
+        if (height[x2] > height[x1]) x1++;
+        else x2--;
+    }
+}
+
+Console.WriteLine(V);
